07c0b29372eea18647e1207642a52aefa4cd3ebd
dhruvpatel2308 <56676428+dhruvpatel2308@users.noreply.github.com>
2020-05-24 17:40:20 +0530

    Delete 5 binarysearch.py

--- assignment2/5 binarysearch.py
+++
@@ -1,6 +0,0 @@
-import bisect
-l1=[1,2,2,2,3,3,3,3,4,4,5,6,7,8,9]
-print(bisect.bisect_left(l1,2))
-print(bisect.bisect_left(l1,3,5,9))
-print(bisect.bisect_right(l1,4,0,8))
-print(bisect.bisect_right(l1,3))
